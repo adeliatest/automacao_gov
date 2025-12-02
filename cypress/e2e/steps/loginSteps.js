16e98a13136919e7d0eced4b7f47f5280ac6aef7
adeliadev <arodriguesthamyres@gmail.com>
2025-12-02 11:38:29 -0300

    remove: Deleta botao de login do arquivo loginSteps

--- cypress/e2e/steps/loginSteps.js
+++ cypress/e2e/steps/loginSteps.js
@@ -2,23 +2,6 @@
 
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
-// Botão de login
-
-Given('que estou na página inicial do gov', () => {
-    cy.visit('https://www.gov.br');
-});
-
-When('clico no botão de login', () => {
-    cy.get('#barra-govbr')
-        .shadow()
-        .find('span.login-text')
-        .click();
-});
-
-Then('a url deverá incluir {string}', (urlEsperada) => {
-    cy.url().should('include', urlEsperada);
-});
-
 // Validação do campo de cpf
 
 Given('que estou na página de login do gov', () => {
